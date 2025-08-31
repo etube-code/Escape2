c9047caddb957d52cc1c8927e068bc7f5c50f2de
etube-code <english.tube@yandex.com>
2025-08-31 17:26:27 +0300

    add bitmap.java

---
+++ src/com/mojang/Escape2/gui/Bitmap.java
@@ -0,0 +1,47 @@
+package com.mojang.Escape2.gui;
+
+
+public class Bitmap {
+	
+	public final int width;
+	public final int height;
+	public final int[] pixels;
+	
+	
+	public Bitmap(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int [width * height];
+		
+	}
+	
+	
+	//Draws the bitmap with x and y offset applied
+	public void draw(Bitmap bitmap, int xOffs, int yOffs) {
+		
+		for (int y=0; y < bitmap.height; y++ ) {			
+			int yPix = y + yOffs;
+			if(yPix<0 || yPix >= height) continue;
+			for (int x=0; x < bitmap.width; x++) {
+				int xPix = x + xOffs;
+				if(xPix<0 || xPix >= width)continue;
+				
+				pixels[xPix + yPix * width] = bitmap.pixels[x + y * bitmap.width];
+				
+				
+				
+			}
+			
+			
+			
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+}
