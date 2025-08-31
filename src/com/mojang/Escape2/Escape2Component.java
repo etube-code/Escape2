45a9eec266412210798ca876b26653933b9feaf2
etube-code <english.tube@yandex.com>
2025-08-31 16:06:16 +0300

    feat: add main window and game component

--- src/com/mojang/Escape2/Escape2Component.java
+++ src/com/mojang/Escape2/Escape2Component.java
@@ -7,43 +7,43 @@ import javax.swing.JFrame;
 
 public class Escape2Component extends JComponent{
 	
+	
 	private static final int WIDTH = 160;
 	private static final int HEIGHT = 120;
 	private static final int SCALE = 4;
 	
 	
-
+	public Escape2Component() {
+		
+		Dimension size = new Dimension(WIDTH*SCALE, HEIGHT*SCALE);
+		setPreferredSize(size);
+		
+		
+	}
 	
 	
-public Escape2Component() {
+	public static void main(String[] args) {
+		
+		Escape2Component game = new Escape2Component();
+		
+		JFrame frame = new JFrame("Escape2..");
+		
+		frame.add(game);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);		
+		
+		
+	}
 	
-	Dimension size = new Dimension(WIDTH*SCALE, HEIGHT*SCALE);
-	setPreferredSize(size);
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
-
-
-
-
-
-public static void main(String[] args) {
-	
-	JFrame frame = new JFrame("Escape..");
-	
-	Escape2Component game = new Escape2Component();
-	
-	
-	frame.add(game);
-	frame.pack();
-	frame.setLocationRelativeTo(null);
-	frame.setVisible(true);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-	
-	
-	
-}
-
-}
-
